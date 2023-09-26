347787ac8a41a9e9161b600834382d34263ba66a
Joshua Mather <jmather0707@gmail.com>
2023-09-26 14:28:32 +0100

    State management - before reducers

--- rn-starter/src/screens/HomeScreen.js
+++ rn-starter/src/screens/HomeScreen.js
@@ -1,34 +1,46 @@
-import React from "react";
-import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
+import React from 'react';
+import { Text, StyleSheet, View, Button, TouchableOpacity } from 'react-native';
 
 const HomeScreen = ({ navigation }) => {
-  return (
-    <View>
-      <Text style={styles.text}>Home Screen!</Text> 
-      {/* // primitive react element */}
-      <Button 
-        title="Go to Components Demo" 
-        onPress={() => navigation.navigate('Components')}
-      />
-      <Button 
-        title="Go to List Demo" 
-        onPress={() => navigation.navigate('List')}
-      />
-      <Button 
-        title="Go to Image Demo" 
-        onPress={() => navigation.navigate('Image')}
-      />
-      {/* <TouchableOpacity onPress={() => props.navigation.navigate('List')}>
+    return (
+        <View>
+            <Text style={styles.text}>Home Screen!</Text>
+            {/* // primitive react element */}
+            <Button
+                title="Go to Components Demo"
+                onPress={() => navigation.navigate('Components')}
+            />
+            <Button
+                title="Go to List Demo"
+                onPress={() => navigation.navigate('List')}
+            />
+            <Button
+                title="Go to Image Demo"
+                onPress={() => navigation.navigate('Image')}
+            />
+            <Button
+                title="Go to Counter Demo"
+                onPress={() => navigation.navigate('Counter')}
+            />
+            <Button
+                title="Go to Colour Demo"
+                onPress={() => navigation.navigate('Colour')}
+            />
+            <Button
+                title="Go to Square Demo"
+                onPress={() => navigation.navigate('Square')}
+            />
+            {/* <TouchableOpacity onPress={() => props.navigation.navigate('List')}>
         <Text>Go to List Demo</Text>
       </TouchableOpacity> */}
-    </View>
-  );
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({
-  text: {
-    fontSize: 30,
-  },
+    text: {
+        fontSize: 30,
+    },
 });
 
 export default HomeScreen;
