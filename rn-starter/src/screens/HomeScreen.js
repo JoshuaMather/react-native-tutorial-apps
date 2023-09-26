9e3af38f224e4998ef92ccb36e8155dfc68d0fcd
Joshua Mather <jmather0707@gmail.com>
2023-09-26 09:59:35 +0100

    Section 4 - Button and Navigation

--- rn-starter/src/screens/HomeScreen.js
+++ rn-starter/src/screens/HomeScreen.js
@@ -1,8 +1,24 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
-const HomeScreen = () => {
-  return <Text style={styles.text}>Home Screen!</Text>; // primitive react element
+const HomeScreen = ({ navigation }) => {
+  return (
+    <View>
+      <Text style={styles.text}>Home Screen!</Text> 
+      {/* // primitive react element */}
+      <Button 
+        title="Go to Components Demo" 
+        onPress={() => navigation.navigate('Components')}
+      />
+      <Button 
+        title="Go to List Demo" 
+        onPress={() => navigation.navigate('List')}
+      />
+      {/* <TouchableOpacity onPress={() => props.navigation.navigate('List')}>
+        <Text>Go to List Demo</Text>
+      </TouchableOpacity> */}
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
