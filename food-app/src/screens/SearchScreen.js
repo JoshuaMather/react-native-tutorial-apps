4bb97a0698b8f9308be87e40aee12c66bade9dcf
Joshua Mather <jmather0707@gmail.com>
2023-09-27 13:19:18 +0100

    Section 9 - Outside API

--- food-app/src/screens/SearchScreen.js
+++ food-app/src/screens/SearchScreen.js
@@ -1,17 +1,40 @@
 import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from './components/SearchBar';
+import yelp from '../api/yelp';
 
 const SearchScreen = () => {
-  const [term, setTerm] = useState('');
-  
-  return (
-    <View>
-      <SearchBar term={term} onTermChange={newTerm => setTerm(newTerm)}
-      onTermSubmit={() => console.log('submit')} />
-      <Text>{term}</Text>
-    </View>
-  );
+    const [term, setTerm] = useState('');
+    const [results, setResults] = useState([]);
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const searchApi = async (searhTerm) => {
+        try {
+            const response = await yelp.get('/search', {
+                params: {
+                    limit: 50,
+                    term: searhTerm,
+                    location: 'san jose',
+                },
+            });
+            setResults(response.data.businesses);
+        } catch (err) {
+            setErrorMessage('Something went wrong');
+        }
+    };
+
+    return (
+        <View>
+            <SearchBar
+                term={term}
+                onTermChange={setTerm}
+                onTermSubmit={() => searchApi(term)}
+            />
+            {/* <Text>{term}</Text> */}
+            {errorMessage ? <Text>{errorMessage}</Text> : null}
+            <Text>We have found {results.length} results</Text>
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({});
