47d905d0af6e664cc480ea7d212945174da1b4d6
Joshua Mather <jmather0707@gmail.com>
2023-10-23 15:10:20 +0100

    Move Status Bar to app.js

--- tracks/App.js
+++ tracks/App.js
@@ -5,6 +5,7 @@ import ResolveAuthScreen from './src/screens/ResolveAuthScreen';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { Provider } from 'react-redux';
 import { store } from './src/store/store';
+import { StatusBar } from 'expo-status-bar';
 
 // if (__DEV__) {
 // import('./ReactotronConfig').then(() => console.log('Reactotron Configured'));
@@ -17,6 +18,12 @@ export default function App() {
     return (
         <SafeAreaProvider>
             <Provider store={store}>
+                <StatusBar
+                    style="dark"
+                    animated={true}
+                    hidden={false}
+                    backgroundColor="#03fcf4"
+                />
                 <ResolveAuthScreen />
                 {/* <App
                                     ref={(navigator) => {
