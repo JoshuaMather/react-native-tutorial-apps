46dc19e0ef7bd604347df682a992d9b5b83659d3
Joshua <jmather0707@gmail.com>
2023-12-22 09:43:17 +0000

    Add scroll view to video screen

--- tracks/src/screens/VideoScreen.tsx
+++ tracks/src/screens/VideoScreen.tsx
@@ -16,6 +16,8 @@ const VideoScreen = () => {
     const [playing, setPlaying] = useState(false);
     const video2 = useRef(null);
     const [playing2, setPlaying2] = useState(false);
+    const video3 = useRef(null);
+    const [playing3, setPlaying3] = useState(false);
     const [result, setResult] = useState(null);
 
     const _handlePressButtonAsync = async () => {
@@ -26,28 +28,31 @@ const VideoScreen = () => {
     return (
         <View>
             <Text className="text-center text-4xl pt-5 font-bold">Video</Text>
-            {/* <ScrollView> */}
-            <Button title="Open YouTube" onPress={_handlePressButtonAsync} />
-            <Text>{result && JSON.stringify(result)}</Text>
-            <View className="items-center">
-                <Text className="text-center text-2xl pt-5">Video #1</Text>
-                <Video
-                    ref={video}
-                    source={{
-                        uri: 'https://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4',
-                    }}
-                    useNativeControls={true}
-                    resizeMode={ResizeMode.CONTAIN}
-                    isLooping
-                    onPlaybackStatusUpdate={(status) => {
-                        console.log(status);
-                        if (status.isLoaded) {
-                            setPlaying(status.isPlaying);
-                        }
-                    }}
-                    className="h-40 w-4/5"
+            <ScrollView>
+                <Button
+                    title="Open YouTube"
+                    onPress={_handlePressButtonAsync}
                 />
-                {/* <VideoPlayer
+                <Text>{result && JSON.stringify(result)}</Text>
+                <View className="items-center">
+                    <Text className="text-center text-2xl pt-5">Video #1</Text>
+                    <Video
+                        ref={video}
+                        source={{
+                            uri: 'https://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4',
+                        }}
+                        useNativeControls={true}
+                        resizeMode={ResizeMode.CONTAIN}
+                        isLooping
+                        onPlaybackStatusUpdate={(status) => {
+                            console.log(status);
+                            if (status.isLoaded) {
+                                setPlaying(status.isPlaying);
+                            }
+                        }}
+                        className="h-40 w-4/5"
+                    />
+                    {/* <VideoPlayer
                         videoProps={{
                             shouldPlay: true,
                             resizeMode: ResizeMode.CONTAIN,
@@ -57,48 +62,78 @@ const VideoScreen = () => {
                             // style: { height: 20 },
                         }}
                     /> */}
-                <Spacer>
-                    <Button
-                        title={playing ? 'Pause' : 'Play'}
-                        onPress={() =>
-                            playing
-                                ? video.current.pauseAsync()
-                                : video.current.playAsync()
-                        }
-                    />
-                </Spacer>
-            </View>
+                    <Spacer>
+                        <Button
+                            title={playing ? 'Pause' : 'Play'}
+                            onPress={() =>
+                                playing
+                                    ? video.current.pauseAsync()
+                                    : video.current.playAsync()
+                            }
+                        />
+                    </Spacer>
+                </View>
 
-            <View className="items-center">
-                <Text className="text-center text-2xl pt-5">Video #2</Text>
-                <Video
-                    ref={video2}
-                    source={{
-                        uri: 'https://ia800501.us.archive.org/11/items/popeye_i_dont_scare/popeye_i_dont_scare_512kb.mp4',
-                    }}
-                    useNativeControls={true}
-                    resizeMode={ResizeMode.CONTAIN}
-                    isLooping
-                    onPlaybackStatusUpdate={(status) => {
-                        console.log(status);
-                        if (status.isLoaded) {
-                            setPlaying2(status.isPlaying);
-                        }
-                    }}
-                    className="h-40 w-4/5"
-                />
-                <Spacer>
-                    <Button
-                        title={playing2 ? 'Pause' : 'Play'}
-                        onPress={() =>
-                            playing2
-                                ? video2.current.pauseAsync()
-                                : video2.current.playAsync()
-                        }
+                <View className="items-center">
+                    <Text className="text-center text-2xl pt-5">Video #2</Text>
+                    <Video
+                        ref={video2}
+                        source={{
+                            uri: 'https://ia800501.us.archive.org/11/items/popeye_i_dont_scare/popeye_i_dont_scare_512kb.mp4',
+                        }}
+                        useNativeControls={true}
+                        resizeMode={ResizeMode.CONTAIN}
+                        isLooping
+                        onPlaybackStatusUpdate={(status) => {
+                            console.log(status);
+                            if (status.isLoaded) {
+                                setPlaying2(status.isPlaying);
+                            }
+                        }}
+                        className="h-40 w-4/5"
                     />
-                </Spacer>
-            </View>
-            {/* </ScrollView> */}
+                    <Spacer>
+                        <Button
+                            title={playing2 ? 'Pause' : 'Play'}
+                            onPress={() =>
+                                playing2
+                                    ? video2.current.pauseAsync()
+                                    : video2.current.playAsync()
+                            }
+                        />
+                    </Spacer>
+                </View>
+
+                <View className="items-center">
+                    <Text className="text-center text-2xl pt-5">Video #3</Text>
+                    <Video
+                        ref={video3}
+                        source={{
+                            uri: 'https://ia800501.us.archive.org/11/items/popeye_i_dont_scare/popeye_i_dont_scare_512kb.mp4',
+                        }}
+                        useNativeControls={true}
+                        resizeMode={ResizeMode.CONTAIN}
+                        isLooping
+                        onPlaybackStatusUpdate={(status) => {
+                            console.log(status);
+                            if (status.isLoaded) {
+                                setPlaying3(status.isPlaying);
+                            }
+                        }}
+                        className="h-40 w-4/5"
+                    />
+                    <Spacer>
+                        <Button
+                            title={playing3 ? 'Pause' : 'Play'}
+                            onPress={() =>
+                                playing3
+                                    ? video3.current.pauseAsync()
+                                    : video3.current.playAsync()
+                            }
+                        />
+                    </Spacer>
+                </View>
+            </ScrollView>
         </View>
     );
 };
